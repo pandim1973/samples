2bd78843d74af265f10dde64001099b7c2ff46a7
pandim1973 <63948339+pandim1973@users.noreply.github.com>
2020-04-19 13:38:35 +0530

    upload
    uploaded

---
+++ build/precache-manifest.38004a253c01d43788f75964925ecd3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778208e05ddc5e5468719b36fcbff56e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a97ced54253501a52ae9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "20a3846a1f40b653c312",
+    "url": "/static/js/2.176a5a1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.176a5a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97ced54253501a52ae9",
+    "url": "/static/js/main.0f5d34d8.chunk.js"
+  },
+  {
+    "revision": "6c65276f3148f41f01e9",
+    "url": "/static/js/runtime-main.65918dce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
